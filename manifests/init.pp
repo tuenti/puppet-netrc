b0dfb8e707b505730fa61c8a70d7e95337c4dfd9
SHaselbauer <sarah.haselbauer@akquinet.de>
2012-08-22 20:16:20 +0200

    fixed template bug, tried several ruby for loop variants, this one was the first one working, because ruby creates one big linear array if an array contains subarrays :-/

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,6 +19,8 @@ class netrc {
 
 define netrc::foruser($root_home_directory="/home",$user, $machine_user_password_triples) {
 	$filename = ".netrc"
+	$prefixes = ['machine ','	login ','	password ']
+	
 	file { "$root_home_directory/$user/$filename":
 		ensure => present,
 		content => template('netrc/netrc.erb'),
