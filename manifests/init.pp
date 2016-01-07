d3ff0ed0e0e2cf51b7b71a9c5f3cdd9925ec1713
Jose Plana <jplana@tuenti.com>
2016-01-07 12:10:23 +0100

    Fixed local references to variablas. Some parameter naming and tabs...

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,14 +17,18 @@ class netrc {
 
 }
 
-define netrc::foruser($root_home_directory="/home",$user, $machine_user_password_triples) {
-	$filename = ".netrc"
-	$prefixes = ['machine ','	login ','	password ']
+define netrc::foruser(
+  $home_directory="/home",
+  $user,
+  $machine_user_password_triples) {
+
+  $filename = ".netrc"
+  $prefixes = ['machine ','	login ','	password ']
 	
-	file { "$root_home_directory/$user/$filename":
-		ensure => present,
-		content => template('netrc/netrc.erb'),
-		mode => '0600',
-		owner => "$user"		
-	}
+  file { "$root_home_directory/$user/$filename":
+    ensure => present,
+    content => template('netrc/netrc.erb'),
+    mode => '0600',
+    owner => "$user"
+  }
 }
