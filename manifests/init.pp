5eb4db578394c0d306e527d27b4c7fd6516010d4
shimon <shimon@tuenti.com>
2020-02-27 13:14:09 +0100

    adding types

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,28 +5,32 @@
 # Parameters: $user is the useraccount on the machine for which the netrc shall be configured, 
 #				$machine_user_password_triples is an array of arrays containing three string params: machine, user, password
 #				$root_home_directory (optional) is the directory where all user homes are located on the target machine, default value is "/home" 
-#
+#       $file_path (optional) is the absolute path of the .netrc file. 
 # Actions:
 #
 # Requires:
 #
 # Sample Usage: netrc::foruser("netrc_myuser": user => 'myuser', machine_user_password_triples => [['myserver.localdomain','myuser','pw'],['mysecondserver.localdomain','myuser','pw2']])
-#               you can also override the path using the `file_path` parameter.
+#               you can also override the full path by using the `file_path` parameter.
 # [Remember: No empty lines between comments and class definition]
 class netrc {
 
 }
 
 define netrc::foruser(
-  Enum["present", "absent"] $ensure = "present",
-  $home_base_directory              = "/home",
-  $user,
-  $filename                         = ".netrc",
-  $file_path                        = "$home_base_directory/$user/$filename",
+  Enum["present", "absent"]     $ensure = "present",
+  Stdlib::Absolutepath          $home_base_directory  = "/home",
+  String                        $user,
+  String                        $filename             = ".netrc",
+  Optional[Stdlib::Absolutepath] $file_path            = "$home_base_directory/$user/$filename",
   $machine_user_password_triples) {
 
+$real_file_path = $file_path ? {
+    undef   => "$home_base_directory/$user/$filename",
+    default => $file_path
+  }
 
-  file { "$file_path":
+  file { "$real_file_path":
     ensure  => $ensure,
     content => template('netrc/netrc.erb'),
     mode    => '0600',
